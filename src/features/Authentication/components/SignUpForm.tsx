6f5657ecc138d40f11ed250ff8e332c2c96a8a3e
hanyang <morksen123@gmail.com>
2024-08-27 10:37:29 +0800

    added sign up form components

---
+++ src/features/Authentication/components/SignUpForm.tsx
@@ -0,0 +1,112 @@
+'use client';
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { signUpFormDefaultValues } from '../constants';
+import { SignUpSchema } from '../schema';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const SignUpForm = () => {
+  const form = useForm({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: signUpFormDefaultValues,
+  });
+
+  const handleUserSignUp = async (data: z.infer<typeof SignUpSchema>) => {
+    console.log('sign up', data);
+  };
+
+  const isFormSubmitting = form.formState.isSubmitting;
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleUserSignUp)}
+        className="space-y-6"
+      >
+        <div className="space-y-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} type="email" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mobile Number</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit" className="w-full" disabled={isFormSubmitting}>
+          {isFormSubmitting ? 'Processing...' : 'Register'}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
---
+++ src/features/Authentication/constants/index.ts
@@ -0,0 +1,12 @@
+export const signInFormDefaultValues = {
+  email: '',
+  password: '',
+};
+
+export const signUpFormDefaultValues = {
+  email: '',
+  name: '',
+  phoneNumber: '',
+  password: '',
+  confirmPassword: '',
+};
