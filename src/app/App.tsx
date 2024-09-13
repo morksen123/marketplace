3deba2bf7e684a06bcebef1371f681d06069092a
hanyang <morksen123@gmail.com>
2024-09-14 01:45:52 +0800

    added jotai and react query providers

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,30 @@
 import { ErrorFallback } from '@/components/errors';
 import { Toaster } from '@/components/ui/toaster';
-import { Suspense } from 'react';
+import { queryConfig } from '@/lib/react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider as JotaiProvider } from 'jotai';
+import { Suspense, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
 import { AppRouter } from './router';
 
 function App() {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      }),
+  );
+
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <ErrorBoundary fallbackRender={ErrorFallback}>
-        <Toaster />
-        <AppRouter />
+        <QueryClientProvider client={queryClient}>
+          <JotaiProvider>
+            <Toaster />
+            <AppRouter />
+          </JotaiProvider>
+        </QueryClientProvider>
       </ErrorBoundary>
     </Suspense>
   );
