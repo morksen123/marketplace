41264ccf55508c8728979fa1eb123f86eb736f53
Han Yang <96196435+morksen123@users.noreply.github.com>
2024-09-24 00:50:20 +0800

    Feat/UI enhancements (#41)
    * added button active state
    
    * used bg-destructive instead of hardcoding green

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,15 +9,16 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'bg-primary text-primary-foreground hover:bg-primary/90 active:bg-primary/80',
         warning:
-          'bg-warning text-warning-foreground hover:bg-warning/80 border-destructive',
+          'bg-warning text-warning-foreground hover:bg-warning/80 border-destructive active:bg-warning/80',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90 active:bg-destructive/80',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/90',
+          'bg-secondary text-secondary-foreground hover:bg-secondary/90 active:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
