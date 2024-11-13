ef2d772e4f6d4de47cb1b5b18c74212a41a8dc66
Spring <springljy12@gmail.com>
2024-11-13 12:16:06 +0800

    Neated up about us page

---
+++ src/app/routes/about.tsx
@@ -0,0 +1,11 @@
+import { LandingNavBar } from "@/features/Sustainability/LandingNavBar/LandingNavBar";
+import { AboutUs } from "@/features/Landing/components/AboutUs";
+
+export const AboutRoute = () => {
+  return (
+    <div>
+      <LandingNavBar />
+      <AboutUs />
+    </div>
+  );
+};
